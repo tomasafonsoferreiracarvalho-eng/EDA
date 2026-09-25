cc54b41554d8a01ae8622d610d4da7645a6e89d5
Tomás Carvalho <tomasafonsoferreiracarvalho@gmail.com>
2026-09-25 15:39:52 +0100

    i don't copy this from the repo of the teacher, ok???😑

--- Ficha1/src/ficha1/Ficha1.java
+++ Ficha1/src/ficha1/Ficha1.java
@@ -14,9 +14,38 @@ public class Ficha1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Math.factorialBigI(0);
         // TODO code application logic here
-        for (int i = 1000; i <=1000; i++)
-            factorialI(i);
+        for(int i = 1000; i <= 20000; i+=1000) {
+            System.out.println(i+" " + runFactoriaIXTimes(10, i) );
+        }
+        
+        System.out.println("------------------------");
+        
+        for(int i = 1000; i <= 20000; i+=1000) {
+            System.out.println(i+" " + runFactoriaRXTimes(10, i) );
+        }
     }
     
+    public static long runFactoriaIXTimes(int x, int num) {
+        long sum=0;
+        for(int i = 0; i < x; i++) {
+            long start = System.nanoTime();
+            Math.factorialBigI(num);
+            long end = System.nanoTime();
+            sum += end-start;
+        }
+        return sum/x;
+    }
+    
+    public static long runFactoriaRXTimes(int x, int num) {
+        long sum=0;
+        for(int i = 0; i < x; i++) {
+            long start = System.nanoTime();
+            Math.factorialBigR(num);
+            long end = System.nanoTime();
+            sum += end-start;
+        }
+        return sum/x;
+    }
 }
