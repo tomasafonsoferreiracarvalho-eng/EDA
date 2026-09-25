da07316b5f9480de7a1e8316358cf0dd1edb2b66
Tomás Carvalho <tomasafonsoferreiracarvalho@gmail.com>
2026-09-25 15:41:03 +0100

    i don't copy this from the repo of the teacher, ok???😑

--- Ficha1/src/ficha1/Math.java
+++ Ficha1/src/ficha1/Math.java
@@ -1,5 +1,7 @@
 package ficha1;
 
+import java.math.BigInteger;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -12,7 +14,7 @@ package ficha1;
 public class Math {
     // 5N + 4 Complexidade Temporal O(N) Linear
     // num, f, i return 4 vars Complixade Espacial S(1) Constante
-    public static int factorialI(int num) {
+    public static long factorialI(int num) {
     int f = 1;                          // 1
     for (int i  = 1; i <= num; i++)     // 1+N+1+N+N 
         f *= i;                         // (1+1)*N = 2N
@@ -21,11 +23,25 @@ public class Math {
     // 5N + 2 Complexidade Temporal o(N) Linear
     //  num, return 2 vars
     // 2 * (N+1) = 2N + 2 vars Complexidade Espacial S(N) Linear
-    public static int factorialR(int num) {
+    public static long factorialR(int num) {
     if (num == 0)                       // 1 *(n+1) = (N+1)
         return 1;                       // 1
     else 
         return (num*factorialR(num-1)); // (1+1+1+1) * N = 4N
     };
+    
+    public static BigInteger factorialBigI(int num) {
+        BigInteger f = BigInteger.ONE;
+        for(int i = 1; i <= num; i++)
+            f = f.multiply(BigInteger.valueOf(i));
+        return f;
+    }
+    
+    public static BigInteger factorialBigR (int num) {
+        if (num == 0)
+            return BigInteger.ONE;
+        else 
+            return factorialBigR(num-1).multiply(BigInteger.valueOf(num));
+    }
     
 }
